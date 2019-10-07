bdc46455be48a005c67cf222c921a222684c0442
IDSRosler <igorsouzarosler@gmail.com>
2019-10-07 15:02:12 -0300

    +- Pronto

--- main.cpp
+++ main.cpp
@@ -43,38 +43,39 @@ void cria_grafo_euclidiano(int **grafo, int n_vertices);
 void imprime_grafo(int **grafo, int n_vertices);
 void destroi_grafo(int **grafo, int n_vertices);
 int TSP_otimo(int **grafo, int vertice_inicial, int V);
+int TSP_aproximado(int **grafo, int vertice_inicial, int n_vertices);
 vector<int> arvore_ger_minima(int **grafo, int vert_inicial, int* arv_min, int n_vertices);
 Node* cria_arvore(vector<int> vetor, int n_vertices, int vert_inicial);
+void full_walk(Node *root, vector<int> *caminho);
 
 int main(int argc, char** argv) {
     int n_vertices = atoi(argv[1]);
     int vertice_inicial = atoi(argv[2]);
+    int op = atoi(argv[3]) == 0 ? 0 : 1;
     int **grafo;
-    int *arv_min = (int *) malloc(n_vertices * sizeof (int));
-    vector<int> vetor;
 
     grafo = init_matriz_adj(n_vertices);
-    grafo[0][1] = 10;
-    grafo[0][2] = 15;
-    grafo[0][3] = 20;
-    grafo[1][0] = 10;
-    grafo[1][2] = 35;
-    grafo[1][3] = 25;
-    grafo[2][0] = 15;
-    grafo[2][1] = 35;
-    grafo[2][3] = 30;
-    grafo[3][0] = 20;
-    grafo[3][1] = 25;
-    grafo[3][2] = 30;
-    //cria_grafo_euclidiano(grafo, n_vertices);
-    imprime_grafo(grafo, n_vertices);
-
-    cout << "\nMenor caminho:\t" << TSP_otimo(grafo, vertice_inicial, n_vertices) << endl;
-    vetor = arvore_ger_minima(grafo, vertice_inicial, arv_min, n_vertices);
-    for (int i = 0; i < n_vertices; i++) {
-        cout << i << "-" << arv_min[i] << " = " << grafo[i][arv_min[i]] << endl;
+//        grafo[0][1] = 10;
+//        grafo[0][2] = 15;
+//        grafo[0][3] = 20;
+//        grafo[1][0] = 10;
+//        grafo[1][2] = 35;
+//        grafo[1][3] = 25;
+//        grafo[2][0] = 15;
+//        grafo[2][1] = 35;
+//        grafo[2][3] = 30;
+//        grafo[3][0] = 20;
+//        grafo[3][1] = 25;
+//        grafo[3][2] = 30;
+    cria_grafo_euclidiano(grafo, n_vertices);
+    //imprime_grafo(grafo, n_vertices);
+    switch (op) {
+        case 0: cout << "Menor caminho:\t" << TSP_otimo(grafo, vertice_inicial, n_vertices) << endl;
+            break;
+        case 1: cout << "Menor caminho aproximado:  " << TSP_aproximado(grafo, vertice_inicial, n_vertices) << endl;
+            break;
     }
-    Node *root = cria_arvore(vetor, n_vertices, vertice_inicial);
+
 
     destroi_grafo(grafo, n_vertices);
 
@@ -98,6 +99,57 @@ int TSP_otimo(int **grafo, int vertice_inicial, int V) {
         caminho_minimo = min(caminho_minimo, tamanho_caminho_atual);
     } while (next_permutation(vertices.begin(), vertices.end()));
     return caminho_minimo;
+}
+
+int TSP_aproximado(int **grafo, int vertice_inicial, int n_vertices) {
+    int *arv_min = (int *) malloc(n_vertices * sizeof (int));
+    int status[n_vertices];
+    int dist = 0;
+    int primeiro = 1;
+    int ultimo = 0;
+    vector<int> vetor, caminho;
+    for (int i = 0; i < n_vertices; i++) {
+        status[i] = -1;
+    }
+    status[vertice_inicial] = 1;
+    vetor = arvore_ger_minima(grafo, vertice_inicial, arv_min, n_vertices);
+    Node *root = cria_arvore(vetor, n_vertices, vertice_inicial);
+    full_walk(root, &caminho);
+    // Mostra caminho percorrido
+    //    cout << "Full Walk:\t";
+    //    for (int i = 0; i < caminho.size(); i++) {
+    //        cout << caminho[i] << " ";
+    //    }
+    //    cout << "\n" << endl;
+    for (int i = 0; i < caminho.size() - 1; i++) {
+        if (status[caminho[i + 1]] == -1 || primeiro == 1) {
+            dist += grafo[caminho[i]][caminho[i + 1]];
+            status[caminho[i]] = 1;
+            if (status[caminho[i]] == -1) {
+                ultimo = caminho[i];
+            }
+            primeiro = 0;
+        } else {
+            for (int j = i; j < caminho.size() - 1; j++) {
+                if (status[caminho[j + 1]] == -1) {
+                    dist += grafo[caminho[i]][caminho[j + 1]];
+                    status[caminho[j]] = 1;
+                    i = j;
+                    if (status[caminho[j]] == -1) {
+                        ultimo = caminho[j];
+                    }
+                    break;
+                } else {
+                    if (status[caminho[j]] == -1) {
+                        ultimo = caminho[j];
+                    }
+                }
+            }
+        }
+    }
+    dist += grafo[ultimo][vertice_inicial];
+
+    return dist;
 }
 
 void cria_grafo_euclidiano(int **grafo, int n_vertices) {
@@ -201,22 +253,33 @@ vector<int> arvore_ger_minima(int **grafo, int vert_inicial, int* arv_min, int n
 }
 
 Node* cria_arvore(vector<int> vetor, int n_vertices, int vert_inicial) {
-    vector<Node> arvore;
+    Node **arvore = (Node**) malloc(n_vertices * sizeof (Node*));
     Node **atual = (Node**) malloc(n_vertices * sizeof (Node*));
     for (int i = 0; i < n_vertices; i++) {
-        Node novo = Node(i);
-        arvore.push_back(novo);
+        //Node novo = Node(i);
+        arvore[i] = new Node(i);
     }
     for (int j = 0; j < n_vertices; j++) {
-        atual[j] = &arvore[j];
+        atual[j] = arvore[j];
         for (int i = 0; i < n_vertices; i++) {
             if (i != j) {
                 if (vetor[i] == atual[j]->index) {
-                    atual[j]->add_node(arvore[i]);
+                    atual[j]->add_node(*arvore[i]);
                 }
             }
         }
     }
-    return &arvore[vert_inicial];
+    return arvore[vert_inicial];
 }
 
+void full_walk(Node *root, vector<int> *caminho) {
+    caminho->push_back(root->index);
+    if (root->prox.size() == 0) {
+        return;
+    } else {
+        for (int i = 0; i < root->prox.size(); i++) {
+            full_walk(&root->prox[i], caminho);
+            caminho->push_back(root->index);
+        }
+    }
+}
