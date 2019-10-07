14289b7e09978b901e69378c63cc6dfcc3990f6a
IDSRosler <igorsouzarosler@gmail.com>
2019-10-06 22:41:21 -0300

    Terminar o algoritmo aproximativo

--- main.cpp
+++ main.cpp
@@ -24,45 +24,58 @@ using namespace std;
 //#define V 8 
 #define MAX 99999999
 
+struct Node {
+    int index;
+    vector<struct Node> prox;
+
+    Node(int index) {
+        this->index = index;
+        this->prox.clear();
+    }
+
+    void add_node(Node node) {
+        this->prox.push_back(node);
+    }
+};
+
 int** init_matriz_adj(int n_vertices);
 void cria_grafo_euclidiano(int **grafo, int n_vertices);
 void imprime_grafo(int **grafo, int n_vertices);
 void destroi_grafo(int **grafo, int n_vertices);
 int TSP_otimo(int **grafo, int vertice_inicial, int V);
-void arvore_ger_minima(int **grafo, int vert_inicial, int *arv_min, int n_vertices);
+vector<int> arvore_ger_minima(int **grafo, int vert_inicial, int* arv_min, int n_vertices);
+Node* cria_arvore(vector<int> vetor, int n_vertices, int vert_inicial);
 
 int main(int argc, char** argv) {
     int n_vertices = atoi(argv[1]);
     int vertice_inicial = atoi(argv[2]);
     int **grafo;
+    int *arv_min = (int *) malloc(n_vertices * sizeof (int));
+    vector<int> vetor;
 
     grafo = init_matriz_adj(n_vertices);
-//    grafo[0][1] = 10;
-//    grafo[0][2] = 15;
-//    grafo[0][3] = 20;
-//    grafo[1][0] = 10;
-//    grafo[1][2] = 35;
-//    grafo[1][3] = 25;
-//    grafo[2][0] = 15;
-//    grafo[2][1] = 35;
-//    grafo[2][3] = 30;
-//    grafo[3][0] = 20;
-//    grafo[3][1] = 25;
-//    grafo[3][2] = 30;
-    cria_grafo_euclidiano(grafo, n_vertices);
+    grafo[0][1] = 10;
+    grafo[0][2] = 15;
+    grafo[0][3] = 20;
+    grafo[1][0] = 10;
+    grafo[1][2] = 35;
+    grafo[1][3] = 25;
+    grafo[2][0] = 15;
+    grafo[2][1] = 35;
+    grafo[2][3] = 30;
+    grafo[3][0] = 20;
+    grafo[3][1] = 25;
+    grafo[3][2] = 30;
+    //cria_grafo_euclidiano(grafo, n_vertices);
     imprime_grafo(grafo, n_vertices);
 
     cout << "\nMenor caminho:\t" << TSP_otimo(grafo, vertice_inicial, n_vertices) << endl;
-
-    int *arv_min = (int *) malloc(n_vertices * sizeof(int));
-    arvore_ger_minima(grafo, vertice_inicial, arv_min, n_vertices);
-    
-    cout << "Arvore minima:\t";
+    vetor = arvore_ger_minima(grafo, vertice_inicial, arv_min, n_vertices);
     for (int i = 0; i < n_vertices; i++) {
-        cout << grafo[arv_min[i]][i] << "\t";
+        cout << i << "-" << arv_min[i] << " = " << grafo[i][arv_min[i]] << endl;
     }
+    Node *root = cria_arvore(vetor, n_vertices, vertice_inicial);
 
-    
     destroi_grafo(grafo, n_vertices);
 
     return 0;
@@ -143,12 +156,13 @@ void destroi_grafo(int **grafo, int n_vertices) {
     free(grafo);
 }
 
-void arvore_ger_minima(int **grafo, int vert_inicial, int *arv_min, int n_vertices) {
-    int i, j, dest, primeiro, menor_dist;
+vector<int> arvore_ger_minima(int **grafo, int vert_inicial, int* arv_min, int n_vertices) {
+    int i, j, dest, primeiro, menor_dist, aux;
     vector<int> vertices;
     for (i = 0; i < n_vertices; i++) {
         arv_min[i] = -1;
     }
+    aux = vert_inicial;
     arv_min[vert_inicial] = vert_inicial;
     while (1) {
         primeiro = 1;
@@ -164,9 +178,8 @@ void arvore_ger_minima(int **grafo, int vert_inicial, int *arv_min, int n_vertic
                             vert_inicial = i;
                             dest = vertices[j];
                             primeiro = 0;
-                        }
-                        else{
-                            if(menor_dist > grafo[i][j]){
+                        } else {
+                            if (menor_dist > grafo[i][j]) {
                                 menor_dist = grafo[i][j];
                                 vert_inicial = i;
                                 dest = vertices[j];
@@ -180,4 +193,30 @@ void arvore_ger_minima(int **grafo, int vert_inicial, int *arv_min, int n_vertic
             break;
         arv_min[dest] = vert_inicial;
     }
+    vector<int> vetor;
+    for (int i = 0; i < n_vertices; i++) {
+        vetor.push_back(arv_min[i]);
+    }
+    return vetor;
 }
+
+Node* cria_arvore(vector<int> vetor, int n_vertices, int vert_inicial) {
+    vector<Node> arvore;
+    Node **atual = (Node**) malloc(n_vertices * sizeof (Node*));
+    for (int i = 0; i < n_vertices; i++) {
+        Node novo = Node(i);
+        arvore.push_back(novo);
+    }
+    for (int j = 0; j < n_vertices; j++) {
+        atual[j] = &arvore[j];
+        for (int i = 0; i < n_vertices; i++) {
+            if (i != j) {
+                if (vetor[i] == atual[j]->index) {
+                    atual[j]->add_node(arvore[i]);
+                }
+            }
+        }
+    }
+    return &arvore[vert_inicial];
+}
+
